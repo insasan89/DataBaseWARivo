ce53be8e79b66406aebda7fd3c1343973140f85c
orne-jpg <ornebrau.psi@gmail.com>
2024-11-18 12:53:59 +0000

    second envio

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router()
+const userRouter = require("./user.router");
+const authRouter = require("./auth.router");
 
-router.use('/user', require('./user'))
 router.use('/movie', require('./movie'))
 router.use('/country', require('./country'))
 router.use('/address', require('./address'))
 router.use('/actor', require('./actor'))
+router.use("/users", userRouter);
+router.use("/auth", authRouter);
 
 module.exports = router
+
+
