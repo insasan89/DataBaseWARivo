dff60c6e421c182768092eeb4d82418751b4e870
Reboot Student <vicentemendezacosta@icloud.com>
2024-12-09 09:56:15 +0000

    Proyecto finalizado

--- react/src/pages/AboutUs.jsx
+++ react/src/pages/AboutUs.jsx
@@ -40,7 +40,7 @@ const AboutUs = () => {
             Brillante escritor decidido a exponer la verdad oculta . Utiliza sus
             habilidades para infiltrarse en sistemas, luchando por la justicia y
             revelando lo que el poder intenta ocultar. Desde las sombras,
-            manipula códigos para asegurar que la verdad salga a la luz,aunque
+            manipula códigos para asegurar que la verdad salga a la luz, aunque
             peligrosa, es la única que puede iluminar la oscuridad.
           </p>
         </div>
